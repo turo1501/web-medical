071f5a6a7abace3e69fb46844d10626820aea791
turo151001 <tuant32456@gmail.com>
2023-05-28 12:03:33 +0700

    update

---
+++ js/order.js
@@ -0,0 +1,27 @@
+const Orders =[
+    {
+        productName:'hasduahdasd',
+        productNumber : '8465',
+        paymentStatus : 'Due',
+        shipping : 'Pending'
+    },
+    {
+        productName:'hasduahdasd',
+        productNumber : '8465',
+        paymentStatus : 'Due',
+        shipping : 'Pending'
+    },
+    {
+        productName:'hasduahdasd',
+        productNumber : '8465',
+        paymentStatus : 'Due',
+        shipping : 'Pending'
+    },
+    {
+        productName:'hasduahdasd',
+        productNumber : '8465',
+        paymentStatus : 'Due',
+        shipping : 'Pending'
+    }
+
+]
